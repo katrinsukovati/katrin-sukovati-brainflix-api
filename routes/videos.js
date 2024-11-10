7688ec01c864a104f49a462638ed31023ace8943
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-10 13:05:58 -0500

    set up first route to get all videos from json file

--- routes/videos.js
+++ routes/videos.js
@@ -6,4 +6,27 @@ import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 const FILE_PATH = "./data/videos.json";
 
+// this is a helper function to read data from JSON file
+const readVideos = () => {
+  try {
+    const videosData = JSON.parse(fs.readFileSync(FILE_PATH, "utf-8"));
+    return videosData;
+  } catch (error) {
+    console.error("Error reading videos data:", error);
+    return [];
+  }
+};
+
+// GET /videos - get the list of videos from the data file
+router.get("/", (_req, res) => {
+  const videosData = readVideos();
+  const videoList = videosData.map(({ id, title, channel, image }) => ({
+    id,
+    title,
+    channel,
+    image,
+  }));
+  res.json(videoList);
+});
+
 export default router;
